7bf4df79424ad2a3cc94e46ac75989ce4edd91b5
Jordan Yaker <jordan.e.yaker@gmail.com>
2012-01-17 21:14:13 -0500

    Added the new location of the compiled backbone files for the project.

---
+++ public/backbone/ninjafund.js
@@ -0,0 +1,14 @@
+/* DO NOT MODIFY. This file was compiled Mon, 16 Jan 2012 21:21:29 GMT from
+ * /home/jordanyaker/Documents/My Projects/ninjafund/app/ninjafund.coffee
+ */
+
+(function() {
+
+  window.NF = {
+    Models: {},
+    Collections: {},
+    Routers: {},
+    Views: {}
+  };
+
+}).call(this);
---
+++ public/backbone/views/security/logon.js
@@ -0,0 +1,47 @@
+/* DO NOT MODIFY. This file was compiled Wed, 18 Jan 2012 02:01:54 GMT from
+ * /home/jordanyaker/Documents/My Projects/ninjafund/app/views/security/logon.coffee
+ */
+
+(function() {
+  var _base,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  (_base = NF.Views).Security || (_base.Security = {});
+
+  NF.Views.Security.LogonView = (function(_super) {
+
+    __extends(LogonView, _super);
+
+    function LogonView() {
+      LogonView.__super__.constructor.apply(this, arguments);
+    }
+
+    LogonView.prototype.template = JST['security/logon'];
+
+    LogonView.prototype.el = 'body';
+
+    LogonView.prototype.initialize = function() {
+      _.bindAll(this);
+      return this.render();
+    };
+
+    LogonView.prototype.destroy = function() {
+      $(this.el).empty();
+      return false;
+    };
+
+    LogonView.prototype.render = function() {
+      $(this.el).html(this.template());
+      return this;
+    };
+
+    LogonView.prototype.events = {
+      'click a.button': 'destroy'
+    };
+
+    return LogonView;
+
+  })(Backbone.View);
+
+}).call(this);
