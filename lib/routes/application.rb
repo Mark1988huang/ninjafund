3255abe60538df37fe668e9a5fbbadbad8cd6264
Jordan Yaker <jordan.e.yaker@gmail.com>
2012-01-29 21:06:13 -0500

    Added the basic authentication checks with Warden.

--- lib/routes/application.rb
+++ lib/routes/application.rb
@@ -1,8 +1,24 @@
 module NinjaFund
   class Application < Base
     get '/' do
-      file = File.join 'public', 'index.html'
-      File.open file 
+      if warden.unauthenticated?
+        session[:return_to] = request.path
+        redirect '/logon'
+      end
+      
+      file = File.join( settings.public_folder, 'index.html' )
+      File.open file
+    end
+    
+    get '/logon' do
+      redirect '/' if warden.authenticated?
+      
+      file = File.join( settings.public_folder, 'logon.html' )
+      File.open file
+    end
+    
+    post '/logon' do
+      # TODO: Handle the authentication of the user using the supplied details.
     end
   end
 end
